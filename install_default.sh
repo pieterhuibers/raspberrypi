14724699955abcb389871e409ecd95bd47577bd3
Pieter Huibers <pieterhuibers@gmail.com>
2018-02-13 21:45:20 +0000

    Initial commit

---
+++ install_default.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install default software
+sudo apt-get install vim
+
+# Set up bash profile
+cp ./.bash_aliases ~/
---
+++ setup_git.sh
@@ -0,0 +1,2 @@
+git config --global user.email "pieterhuibers@gmail.com"
+git config --global user.name "Pieter Huibers"
