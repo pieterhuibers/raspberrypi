d19a8998f6687e180872806e21cb773d11f94b2c
Pieter Huibers <pieterhuibers@gmail.com>
2018-02-13 22:08:23 +0000

    Add ctop to docker install script

--- install_docker.sh
+++ install_docker.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+# Install docker
 curl -sSL https://get.docker.com | sh
+
+# Use docker as non-root user
+sudo usermod -aG docker pi
+
+# Install ctop
+sudo wget https://github.com/bcicen/ctop/releases/download/v0.7/ctop-0.7-linux-arm -O /usr/local/bin/ctop
+sudo chmod +x /usr/local/bin/ctop
