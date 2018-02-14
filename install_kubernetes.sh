edeaf2d576ef8968f436651a6b82eeb298180d64
Pieter Huibers <pieterhuibers@gmail.com>
2018-02-14 14:56:13 +0000

    Fix for /boot/cmdline.txt

--- install_kubernetes.sh
+++ install_kubernetes.sh
@@ -7,7 +7,7 @@ sudo apt-get update
 sudo apt-get install -y kubeadm kubectl kubelet kubernetes-cni
 
 # enable cgroups memory and disable swap
-echo "dwc_otg.lpm_enable=0 console=serial0,115200 console=tty1 root=PARTUUID=83c04474-02 rootfstype=ext4 elevator=deadline fsck.repair=yes rootwait cgroup_enable=memory cgroup_memory=1" | sudo tee /boot/cmdline.txt
+echo `cat /boot/cmdline.txt` cgroup_enable=memory cgroup_memory=1 | sudo tee /boot/cmdline.txt
 sudo systemctl disable dphys-swapfile
 
 echo "Restart system for changes to take effect: sudo reboot"
