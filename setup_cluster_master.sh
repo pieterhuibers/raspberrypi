128ede3e509c4643916a1b7a3e513a48996fc033
Pieter Huibers <pieterhuibers@gmail.com>
2018-02-14 20:05:15 +0000

    Add kube-dns (weaveworks)

--- setup_cluster_master.sh
+++ setup_cluster_master.sh
@@ -5,3 +5,5 @@ sudo kubeadm init
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply --filename https://git.io/weave-kube-1.6
